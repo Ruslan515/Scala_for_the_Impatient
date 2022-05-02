116a791750cc831854ee340ba7200293e039f248
Omega515 <ruslan515yulaev@yandex.ru>
2022-05-02 08:46:34 +0500

    ch09 t04

--- src/main/scala/chapter_09/task_04.scala
+++ src/main/scala/chapter_09/task_04.scala
@@ -1,5 +1,16 @@
+/*
+Напишите программу на Scala, которая читает текстовый
+файл, содержащий только вещественные числа, выводит
+сумму, среднее, максимальное и минимальное значения.
+ */
 package chapter_09
+import scala.io.Source
 
-object task_04 {
+object task_04 extends App {
+    val file_name = "src/main/scala/chapter_09/for_04.txt"
+    val source = Source.fromFile(file_name)
+    val tokens = source.mkString.split("\\s+")
+    val numbers = tokens.map(_.toDouble)
+    println(f"sum = ${numbers.sum}, max = ${numbers.max}, min = ${numbers.min}")
 
 }
